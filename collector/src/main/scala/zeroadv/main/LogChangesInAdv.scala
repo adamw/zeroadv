9ddd2d66a65d1749083d805ed85241e8a5c3b6d7
adamw <adam@warski.org>
2014-01-30 16:50:00 +0100

    Logger for changes only

---
+++ collector/src/main/scala/zeroadv/main/LogChangesInAdv.scala
@@ -0,0 +1,14 @@
+package zeroadv.main
+
+import zeroadv.ZeroadvSubscriber
+
+object LogChangesInAdv extends App {
+  private var last: List[Byte] = Nil
+
+  new ZeroadvSubscriber({ adv =>
+    if (last != adv.adv.toList) {
+      last = adv.adv.toList
+      println(adv.whenStr + ": " + adv.advStr)
+    }
+  }).subscribe("tcp://pi1:8916")
+}
