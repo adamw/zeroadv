8d88eac77708546b33d7a3ebcbb61a9cf9f9a1e7
adamw <adam@warski.org>
2014-04-20 20:26:50 +0200

    Training NN

---
+++ collector/src/main/scala/zeroadv/position/nn/NNConfig.scala
@@ -0,0 +1,6 @@
+package zeroadv.position.nn
+
+trait NNConfig {
+  val agentCount = 3
+  val spottingsPerAgent = 3
+}
--- collector/src/main/scala/zeroadv/position/nn/TrainNN.scala
+++ collector/src/main/scala/zeroadv/position/nn/TrainNN.scala
@@ -10,8 +10,15 @@ import zeroadv.ReceivedAdv
 import zeroadv.filter.IncludeOnlyLightGreenBeacon
 import zeroadv.position.ReceivedAdvParser
 import com.typesafe.scalalogging.slf4j.Logging
+import scala.util.Random
+import org.encog.neural.networks.layers.BasicLayer
+import org.encog.neural.networks.BasicNetwork
+import org.encog.engine.network.activation.ActivationSigmoid
+import org.encog.ml.data.basic.BasicMLDataSet
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation
+import org.encog.Encog
 
-object TrainNN extends App with DbModule with IncludeOnlyLightGreenBeacon with Logging {
+object TrainNN extends App with DbModule with IncludeOnlyLightGreenBeacon with NNConfig with Logging {
   lazy val system = ActorSystem()
 
   lazy val receivedAdvParser = wire[ReceivedAdvParser]
@@ -23,15 +30,13 @@ object TrainNN extends App with DbModule with IncludeOnlyLightGreenBeacon with L
     .sortBy(_.when.getMillis)
     .foldLeft((Option.empty[PosM], Map[PosM, List[BeaconSpotting]]())) { case ((currentPos, acc), event) =>
     (currentPos, event) match {
-      case (Some(pos), ev: ReceivedAdv) => {
+      case (Some(pos), ev: ReceivedAdv) =>
         receivedAdvParser.parse(ev) match {
-          case Some(bs) if includeBeaconSpotting(bs) => {
+          case Some(bs) if includeBeaconSpotting(bs) =>
             val updated = pos -> (bs :: acc.getOrElse(pos, Nil))
             (Some(pos), acc + updated)
-          }
           case _ => (Some(pos), acc)
         }
-      }
       case (Some(pos), ev: EndMark) => (None, acc)
       case (Some(pos), ev: MarkPosition) => throw new IllegalStateException()
       case (None, ev: ReceivedAdv) => (None, acc)
@@ -46,6 +51,86 @@ object TrainNN extends App with DbModule with IncludeOnlyLightGreenBeacon with L
     logger.info(s"   $pos -> ${spottings.size}")
   }
 
+  val minCoord = -1.0d
+  val maxCoord = 5.5d
+
+  val minScaled = 0.0d
+  val maxScaled = 1.0d
+
+  def scaleFromCoord(coord: DimM): Double = (coord.coord - minCoord) / (maxCoord - minCoord) * (maxScaled - minScaled) + minScaled
+  def scaleToCoord(scaled: Double): DimM = DimM((scaled - minScaled) / (maxScaled - minScaled) * (maxCoord - minCoord) + minCoord)
+
+  case class TrainingExample(input: Map[Agent, List[TimedRssi]], output: PosM) {
+    def inputToDoubleArray = input
+      .toList
+      .sortBy(_._1.name)
+      .flatMap(_._2.sortBy(_.when.getMillis))
+      .map(_.rssi.toDouble)
+      .toArray
+
+    def outputToDoubleArray = Array(scaleFromCoord(output.x), scaleFromCoord(output.y))
+  }
+
+  val allExamples = positionsToSpottings.flatMap { case (pos, spottings) =>
+    spottings.foldLeft((BeaconsSpottings(Map()), List[TrainingExample]())) { case ((beaconsSpottings, acc), spotting) =>
+      val (beaconSpottings, newBeaconsSpottings) = beaconsSpottings.addSpotting(spotting, spottingsPerAgent)
+      val newExample = if (beaconSpottings.history.size == agentCount && beaconSpottings.history.forall(_._2.size == spottingsPerAgent)) {
+        Some(TrainingExample(beaconSpottings.history, pos))
+      } else {
+        None
+      }
+      (newBeaconsSpottings, newExample.map(_ :: acc).getOrElse(acc))
+    }._2
+  }
+
+  logger.info("Number of training examples: " + allExamples.size)
+
+  val random = new Random()
+  val (trainingExamples, testExamples) = random.shuffle(allExamples).splitAt((allExamples.size * 0.9).toInt)
+
+  //
+
+  val network = new BasicNetwork()
+  network.addLayer(new BasicLayer(null, true, agentCount * spottingsPerAgent))
+  network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 20))
+  network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 2))
+  network.getStructure.finalizeStructure()
+  network.reset()
+
+  val trainingSet = new BasicMLDataSet(
+    trainingExamples.map(_.inputToDoubleArray).toArray,
+    trainingExamples.map(_.outputToDoubleArray).toArray
+  )
+
+  val train = new ResilientPropagation(network, trainingSet)
+
+  var epoch = 1
+
+  do {
+    train.iteration()
+    println("Epoch #" + epoch + " Error:" + train.getError)
+    epoch += 1
+  } while (epoch <= 1000)
+  train.finishTraining()
+
+  for (testExample <- testExamples) {
+    val output = Array.ofDim[Double](2)
+    network.compute(testExample.inputToDoubleArray, output)
+    println("%.4f -> %.4f, %.4f -> %.4f (%.4f, %.4f)".format(
+      testExample.output.x.coord, scaleToCoord(output(0)).coord,
+      testExample.output.y.coord, scaleToCoord(output(1)).coord,
+      output(0), output(1)))
+  }
+
+  println("Train error: " + train.getError)
+  println("Test error: " + network.calculateError(new BasicMLDataSet(
+    testExamples.map(_.inputToDoubleArray).toArray,
+    testExamples.map(_.outputToDoubleArray).toArray
+  )))
+
+  //
+
+  Encog.getInstance().shutdown()
   system.shutdown()
   system.awaitTermination()
   sys.exit(0)
