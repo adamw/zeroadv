afa5f4d5603325311fc48fc707df8a2626179483
adamw <adam@warski.org>
2014-04-22 16:14:46 +0200

    Config

--- collector/src/main/scala/zeroadv/position/nn/NNModule.scala
+++ collector/src/main/scala/zeroadv/position/nn/NNModule.scala
@@ -3,7 +3,7 @@ package zeroadv.position.nn
 import zeroadv.AgentSetup
 
 trait NNModule extends AgentSetup {
-  lazy val nnConfig = NNConfig(agents.agents.size, 4, List(30))
+  lazy val nnConfig = NNConfig(agents.agents.size, 4, List(80))
   lazy val nnOutputScaling = new NNOutputScaling(minDim.coord, maxDim.coord)
   def loadNNFromFile = NN.loadFromFile(nnOutputScaling, nnConfig)
 }
